6ab903984caab7b4d99f11507b67ce0a158fbacb
Evaristo GZ <evgarciaz@gmail.com>
2014-05-22 00:15:43 +0200

    Modificada la ruta de TEMPLATE_PATH

--- wsgi/mybottleapp.py
+++ wsgi/mybottleapp.py
@@ -4,23 +4,20 @@ print "tuitgenerator || Generar código HTML para acciones en Twitter"
 
 @route('/hola')
 def hello():
-    return "Hello World! OK"
+	return "Hello World! OK"
 
 @get('/')
 def index():
 	return template('index.tpl')
 
 # This must be added in order to do correct path lookups for the views
-import os
-from bottle import TEMPLATE_PATH
-
 ON_OPENSHIFT = False
 if os.environ.has_key('OPENSHIFT_REPO_DIR'):
     ON_OPENSHIFT = True
 
 if ON_OPENSHIFT:
     TEMPLATE_PATH.append(os.path.join(os.environ['OPENSHIFT_HOMEDIR'], 
-                                      'runtime/repo/wsgi/views/'))
+                                      'app-root/repo/wsgi/views/'))
     
     application=default_app()
 else:
