383fa4b3a575d0e8d8028e643680cbede3e73da3
Evaristo GZ <evgarciaz@gmail.com>
2014-05-28 12:55:38 +0200

    Añadido al fichero funciones.py las distintas funciones para crear la URI

--- wsgi/funciones.py
+++ wsgi/funciones.py
@@ -1,21 +1,48 @@
+# -*- coding: utf-8 -*-
+
 def tweet():
 	link = "https://twitter.com/intent/tweet?"
-	if len(texto) != 0:
-		text = "&text="+texto
-		link = link+texto
+	if len(text) != 0:
+		text = "&text="+text
 	if len(url) != 0:
 		url = "&url="+url
-		link = link+url
 	if len(via) != 0:
 		via = "&via="+via
-		link = link+via
 	if len(url) != 0:
 		in_reply_to = "&in_reply_to="+in_reply_to
-		link = link+in_reply_to
 	if len(hashtags) != 0:
 		hashtags = "&hashtags="+hashtags
-		link = link+hashtags
 	if len(related) != 0:
 		related = "&related="+related
-		link = link+related
+
+	parametros = text+url+via+in_reply_to+hashtags+related
+	print link+parametros
 	return link
+
+# def retweet():
+# 	link = "https://twitter.com/intent/retweet?"
+# 	if len(tweet_id) != 0:
+# 		tweet_id = "&tweet_id="+tweet_id
+# 	if len(related) != 0:
+# 		related = "&related="+related
+
+# 	parametros = tweet_id+related
+# 	print link+parametros
+
+# def favorite():
+# 	link = "https://twitter.com/intent/favorite?"
+# 	if len(tweet_id) != 0:
+# 		tweet_id = "&tweet_id="+tweet_id
+# 	if len(related) != 0:
+# 		related = "&related="+related
+
+# 	parametros = tweet_id+related
+# 	print link+parametros
+
+# def user():
+# 	link = "https://twitter.com/intent/user?"
+# 	if len(screen_name) != 0:
+# 		screen_name = "&screen_name="+screen_name
+
+# 	parametros = screen_name
+# 	print link+parametros
