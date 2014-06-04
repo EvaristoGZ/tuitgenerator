16dee4d39c08bbd3d9b109520379271d5dea9d30
Evaristo GZ <evgarciaz@gmail.com>
2014-06-05 01:38:24 +0200

    Modificadas las funciones que ejercen las distintas tareas

--- wsgi/funciones.py
+++ wsgi/funciones.py
@@ -19,30 +19,29 @@ def tweet(text,url,via,in_reply_to,hashtags,related):
 	parametros = text+url+via+in_reply_to+hashtags+related
 	return link+parametros
 
-# def retweet():
-# 	link = "https://twitter.com/intent/retweet?"
-# 	if len(tweet_id) != 0:
-# 		tweet_id = "&tweet_id="+tweet_id
-# 	if len(related) != 0:
-# 		related = "&related="+related
+def retweet(tweet_id,):
+	link = "https://twitter.com/intent/retweet?"
+	if len(tweet_id) != 0:
+		tweet_id = "&tweet_id="+tweet_id
+	if len(related) != 0:
+		related = "&related="+related
 
-# 	parametros = tweet_id+related
-# 	print link+parametros
+	parametros = tweet_id+related
+	return link+parametros
 
-# def favorite():
-# 	link = "https://twitter.com/intent/favorite?"
-# 	if len(tweet_id) != 0:
-# 		tweet_id = "&tweet_id="+tweet_id
-# 	if len(related) != 0:
-# 		related = "&related="+related
+def favorite():
+	link = "https://twitter.com/intent/favorite?"
+	if len(tweet_id) != 0:
+		tweet_id = "&tweet_id="+tweet_id
+	if len(related) != 0:
+		related = "&related="+related
 
-# 	parametros = tweet_id+related
-# 	print link+parametros
+	parametros = tweet_id+related
+	return link+parametros
 
-# def user():
-# 	link = "https://twitter.com/intent/user?"
-# 	if len(screen_name) != 0:
-# 		screen_name = "&screen_name="+screen_name
+def user():
+	link = "https://twitter.com/intent/user?"
+	if len(screen_name) != 0:
+		screen_name = "&screen_name="+screen_name
 
-# 	parametros = screen_name
-# 	print link+parametros
+	return link+screen_name
